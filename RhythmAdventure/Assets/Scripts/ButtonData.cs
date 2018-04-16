9540fdb8b16c25978d4ed29fc0411d20e2a65f7e
DanSalib <dansalib@outlook.com>
2018-04-15 20:04:52 -0400

    setup button creation based on json

---
+++ RhythmAdventure/Assets/Scripts/ButtonData.cs
@@ -0,0 +1,12 @@
+﻿[System.Serializable]
+public class ButtonData
+{
+    public ButtonItem[] buttons;
+}
+
+[System.Serializable]
+public class ButtonItem
+{
+    public float time;
+    public float[] position = new float[2];
+}
--- RhythmAdventure/Assets/Scripts/GameController.cs
+++ RhythmAdventure/Assets/Scripts/GameController.cs
@@ -1,30 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
 using UnityEngine;
 using UnityEngine.UI;
-using System.Diagnostics;
+
+
 
 public class GameController : MonoBehaviour {
 
     public GameObject buttonPrefab;
     public Text scoreLabel;
 
-    public List<float> buttonStartTimes;
-    public List<float> buttonXPositions;
-    public List<float> buttonYPositions;
+    private List<float> buttonStartTimes = new List<float>();
+    private List<float> buttonXPositions = new List<float>();
+    private List<float> buttonYPositions = new List<float>();
+
+    public Dictionary<float, float[]> buttonMap = new Dictionary<float, float[]>();
 
     public float gameSpeed;
     public int gameScore = 0;
-
     public Stopwatch gameTimer = new Stopwatch();
 
     ButtonController buttonController;
 
+    private string gameDataFileName = "data.json";
+
+    private
+
 	// Use this for initialization
 	void Start () {
+        if(!LoadGameData())
+        {
+            return;
+        }
+
         gameTimer.Start();
         ButtonController.OnClicked += OnGameButtonClick;
         
+    }
+
+    private bool LoadGameData()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+
+        if(File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+
+            ButtonData buttonData = JsonUtility.FromJson<ButtonData>(dataAsJson);
+
+            for (int i = 0; i < buttonData.buttons.Length; ++i)
+            {
+
+                UnityEngine.Debug.Log("button time = " + buttonData.buttons[i].time);
+                UnityEngine.Debug.Log("button pos = " + buttonData.buttons[i].position[0]);
+                UnityEngine.Debug.Log("button pos2 = " + buttonData.buttons[i].position[1]);
+
+                buttonStartTimes.Add(buttonData.buttons[i].time);
+
+                buttonXPositions.Add(buttonData.buttons[i].position[0]);
+
+                buttonYPositions.Add(buttonData.buttons[i].position[1]);
+            }
+
+            return true;
+        }
+
+        return false;
     }
 	
 	// Update is called once per frame
